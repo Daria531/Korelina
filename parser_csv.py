b85c88c2f0c96e23d7bd19d51ad4f7abf922896a
Daria531 <daria.korelina@urfu.me>
2022-12-29 15:51:00 +0500

    Multiproc

---
+++ parser_csv.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+pd.set_option("display.max_columns", False)
+pd.set_option("expand_frame_repr", False)
+
+def parse_csv_by_year(file="C:\\Users\\RBT\\PycharmProjects\\Korelina\\.idea\\Files\\vacancies_by_year.csv"):
+    """
+    Группирует и разделяет данные во входном файле по годам
+    :param file: str Путь к файлу
+    """
+    df = pd.read_csv(file)
+    df["year"] = df["published_at"].apply(lambda s: s[:4])
+    df = df.groupby("year")
+    for year, data in df:
+        data[["name",
+              "salary_from",
+              "salary_to",
+              "salary_currency",
+              "area_name",
+              "published_at"]].to_csv(rf"C:\Users\RBT\PycharmProjects\Korelina\.idea\Csvs\year_{year}.csv", index=False)
+#parse_csv_by_year()
+
